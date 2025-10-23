d256d51e49e204fdcbf5db3becfae987c06c4270
sreejansandhani <sreejansandhani@gmail.com>
2025-10-23 14:50:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
   navLinks.classList.toggle('show');
 });
 
